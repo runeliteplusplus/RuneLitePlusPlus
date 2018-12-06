38eb365fa16fefc1e25e74c03e34cf1b09c6dfd1
runeliteplusplus <43920963+runeliteplusplus@users.noreply.github.com>
2018-12-06 11:44:28 -0600

    Update FreezeTimersPlugin.java
    Move EventBus implementation to runelite-client

--- freezetimers/FreezeTimersPlugin.java
+++ freezetimers/FreezeTimersPlugin.java
@@ -1,6 +1,6 @@
 package net.runelite.client.plugins.freezetimers;
 
-import com.google.common.eventbus.Subscribe;
+import net.runelite.client.eventbus.Subscribe;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import java.awt.*;
